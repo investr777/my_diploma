2caa504bdab8dfcc049becb285762e9e0d27c1b5
Darkin_rall <juridov.mask@gmail.com>
2019-02-27 14:28:05 +0300

    Add and delete service by phone

---
+++ phone/src/main/java/com/example/juridov/phone/controller/user/ServicePhoneUserController.java
@@ -0,0 +1,54 @@
+package com.example.juridov.phone.controller.user;
+
+import com.example.juridov.phone.entity.ServicePhone;
+import com.example.juridov.phone.entity.User;
+import com.example.juridov.phone.service.PhoneService;
+import com.example.juridov.phone.service.ServicePhoneService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user/service", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "USER API", description = "User REST Controller API, manage services of this phone")
+@PreAuthorize("hasAuthority('USER')")
+public class ServicePhoneUserController {
+    private final ServicePhoneService servicePhoneService;
+    private final PhoneService phoneService;
+
+    @Autowired
+    public ServicePhoneUserController(ServicePhoneService servicePhoneService, PhoneService phoneService) {
+        this.servicePhoneService = servicePhoneService;
+        this.phoneService = phoneService;
+    }
+
+    @ApiOperation(value = "Get a list services of this phone number", response = ServicePhone.class)
+    @RequestMapping(method = RequestMethod.GET)
+    public List<ServicePhone> getInformation(@AuthenticationPrincipal User user) {
+        Long phoneId = phoneService.getPhoneByUser(user.getId()).getId();
+        return servicePhoneService.getListOfPhone(phoneId);
+    }
+
+    @ApiOperation(value = "Add a new service to this phone", response = ServicePhone.class)
+    @RequestMapping(path = "/{serviceId}", method = RequestMethod.POST)
+    public ServicePhone addServiceToPhone(@AuthenticationPrincipal User user, @PathVariable Long serviceId) throws Exception {
+        Long phoneId = phoneService.getPhoneByUser(user.getId()).getId();
+        return servicePhoneService.addServiceToPhone(phoneId, serviceId);
+    }
+
+    @ApiOperation(value = "Delete a service from this phone", response = ServicePhone.class)
+    @RequestMapping(path = "/{servicePhoneId}", method = RequestMethod.DELETE)
+    public void deleteServiceFromPhone(@AuthenticationPrincipal User user, @PathVariable Long servicePhoneId) {
+        Long phoneId = phoneService.getPhoneByUser(user.getId()).getId();
+        servicePhoneService.deleteServiceFromPhone(servicePhoneId, phoneId);
+    }
+}
--- phone/src/main/java/com/example/juridov/phone/service/ServicePhoneService.java
+++ phone/src/main/java/com/example/juridov/phone/service/ServicePhoneService.java
@@ -2,6 +2,7 @@ package com.example.juridov.phone.service;
 
 import com.example.juridov.phone.entity.ServicePhone;
 import com.example.juridov.phone.repository.ServicePhoneRepository;
+import com.example.juridov.phone.repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,12 @@ import java.util.List;
 @Service
 public class ServicePhoneService {
     private final ServicePhoneRepository servicePhoneRepository;
+    private final ServiceRepository serviceRepository;
 
     @Autowired
-    public ServicePhoneService(ServicePhoneRepository servicePhoneRepository) {
+    public ServicePhoneService(ServicePhoneRepository servicePhoneRepository, ServiceRepository serviceRepository) {
         this.servicePhoneRepository = servicePhoneRepository;
+        this.serviceRepository = serviceRepository;
     }
 
     public List<ServicePhone> getFullListPhoneService() {
@@ -24,16 +27,31 @@ public class ServicePhoneService {
         return servicePhoneRepository.findAllByPhoneId(phoneId);
     }
 
-    public ServicePhone addServiceToPhone(ServicePhone servicePhone) {
+    public ServicePhone addServiceToPhone(Long phoneId, Long serviceId) throws Exception {
+        if (serviceRepository.findServiceById(serviceId) == null) {
+            return null;
+        }
+        for (ServicePhone servicePhone : servicePhoneRepository.findAllByPhoneId(phoneId)) {
+            if (servicePhone.getServiceId() == serviceId) {
+                throw new Exception("Already connected");
+            }
+        }
+        ServicePhone servicePhone = new ServicePhone();
+        servicePhone.setPhoneId(phoneId);
+        servicePhone.setServiceId(serviceId);
         return servicePhoneRepository.save(servicePhone);
     }
 
-    public void deleteServiceFromPhone(Long servicePhoneId) {
+    public void deleteServiceFromPhone(Long servicePhoneId, Long phoneId) {
         ServicePhone servicePhoneFromDB = servicePhoneRepository.findServicePhoneById(servicePhoneId);
         if (servicePhoneFromDB == null) {
             return;
         }
-        servicePhoneRepository.delete(servicePhoneFromDB);
+        for (ServicePhone servicePhone : servicePhoneRepository.findAllByPhoneId(phoneId)) {
+            if (servicePhone.getId() == servicePhoneId) {
+                servicePhoneRepository.delete(servicePhoneFromDB);
+            }
+        }
     }
 
     public void deleteServicePhone(Long serviceId) {
